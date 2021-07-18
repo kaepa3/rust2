4f5761ec64e29833d6a188e6a98be00c98cd4714
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-07-18 16:03:44 +0900

    finish ch2

--- ch2/guessing_game/src/main.rs
+++ ch2/guessing_game/src/main.rs
@@ -10,19 +10,27 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Faild to read line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line!");
 
-    let guess:u32=guess.trim().parse().expect("Please type a numbuer");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed:{}", guess);
+        println!("You guessed:{}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
