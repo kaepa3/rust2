fda28c9236c798e2d80f404d0198b35da7d44a4b
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-08-28 17:15:51 +0900

    new write

--- ch3/variables/src/main.rs
+++ ch3/variables/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut x = 5;
-    println!("the value of x is:{}", x);
-    x = 6;
-    println!("the value of x is:{}", x);
+    let x = 5;
+    let x = x+1;
+    let x = x*2;
+    println!("the value of x is: {}", x);
 }
