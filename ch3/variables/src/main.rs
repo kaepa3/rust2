ab26fb0a352705b81212ae6ba72af6089a445e9c
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-08-28 17:19:31 +0900

    type construct

--- ch3/variables/src/main.rs
+++ ch3/variables/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
     let x = 5;
-    let x = x+1;
-    let x = x*2;
+    let x = x + 1;
+    let x = x * 2;
     println!("the value of x is: {}", x);
 
     let spaces = "    ";
     let spaces = spaces.len();
+    println!("space len:{}", spaces);
+
+    let guess: u32 = "42".parse().expect("Not a number!");
 }
